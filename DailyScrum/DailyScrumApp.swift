826679e2fd6c9144133052789b9c3c0c00b3b3f9
Mohammed Aabid A <mohammed@mavencart.com>
2022-05-05 21:25:10 +0530

    Initial Commit

---
+++ DailyScrum/DailyScrumApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyScrumApp.swift
+//  DailyScrum
+//
+//  Created by Mohammed aabid on 05/05/22.
+//
+
+import SwiftUI
+
+@main
+struct DailyScrumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
